dc709dc3dbba373a5f14f04ff4e3e28c94101c1e
admin <maximsushkin02@gmail.com>
2022-03-22 16:57:05 +0300

    Decomposition of the project into reactive components

---
+++ src/components/ContentTitle.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContentTitle = () => {
+  return <h2 className="content__title">Все пиццы</h2>;
+};
+
+export default ContentTitle;
---
+++ src/components/ContentTop.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Categories from "./Categories";
+import Sort from "./Sort";
+
+const ContentTop = () => {
+  return (
+    <div className="content__top">
+      <Categories />
+      <Sort />
+    </div>
+  );
+};
+
+export default ContentTop;
